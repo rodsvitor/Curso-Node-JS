8af7750163ddee212e619e732c6f8c4bf741b835
Rodrigo Vitor <rodrigovitor@maxtool.com.br>
2020-06-05 20:27:06 -0300

    Adicionando CORS

--- node-api/server.js
+++ node-api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 //iniciando o App
 const app = express();
 app.use(express.json());//Permitir receber dados em formato JSON
+app.use(cors()); //liberando api para ser acessada publicamente.
 
 //Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi',
