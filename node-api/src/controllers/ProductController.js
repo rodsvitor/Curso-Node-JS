3872f7b6339420e43e58c448a0b713a22c59aa93
Rodrigo Vitor <rodrigovitor@maxtool.com.br>
2020-06-05 20:07:08 -0300

    configurando paginate

--- node-api/src/controllers/ProductController.js
+++ node-api/src/controllers/ProductController.js
@@ -6,7 +6,10 @@ const Product = mongoose.model('Product');
 //res: resposta que o servidor dá ao cliente.
 module.exports = {
   async index(req, res) {
-    const products = await Product.find();
+    const { page = 1 } = req.query;
+
+    // const products = await Product.find();
+    const products = await Product.paginate({}, { page, limit: 10 });
     return res.json(products);
   },
 
