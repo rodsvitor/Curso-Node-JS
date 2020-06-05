1dff85f3db2b949c6d6ed9c3f942a389394c4d86
Rodrigo Vitor <rodrigovitor@maxtool.com.br>
2020-06-05 11:08:07 -0300

    Primeira rota

--- node-api/server.js
+++ node-api/server.js
@@ -5,7 +5,6 @@ const app = express();
 //req: é a requisição do cliente, por onde vem todos os parâmetros:
 //   corpo, cabeçalho, usuário, autenticação, ip, etc.
 //res: resposta que o servidor dá ao cliente.
-
 app.get('/', (req, res) => res.send("Hello Pau no cú."))
 
 app.listen(3001);
